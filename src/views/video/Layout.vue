8c0711d811598b60bf2b8074f2e2b3db87d824b1
nono-wei <56439015+nono-wei@users.noreply.github.com>
2019-11-28 17:07:05 +0800

    项目初始化

---
+++ src/views/video/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='container'>
+    <!-- 顶部导航 -->
+
+    <!-- 底部Tab栏 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style lang="less" scoped>
+</style>
